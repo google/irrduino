d53aabfa5600e96da7b390ac5f7659dc1fc98f9d
Joe Fernandez <joefernandez@67-218-107-137.cust.layer42.net>
2011-11-04 17:55:15 -0700

    code cleanup

--- IrrduinoDroid/IrrduinoRemote/src/com/joefernandez/irrduino/android/remote/IrrduinoRemoteActivity.java
+++ IrrduinoDroid/IrrduinoRemote/src/com/joefernandez/irrduino/android/remote/IrrduinoRemoteActivity.java
@@ -19,7 +19,6 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
-import android.widget.Toast;
 
 import com.joefernandez.irrduino.android.remote.R.id;
 
