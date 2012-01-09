adc298cb3dce6a5aa863ee4cf22b7eefa9645cb6
Shannon -jj Behrens <jjinux@gmail.com>
2012-01-09 12:08:10 -0800

    Added a droid image

--- IrrduinoServer/irrduinoserver/utils/irrduino.py
+++ IrrduinoServer/irrduinoserver/utils/irrduino.py
@@ -29,13 +29,34 @@ MAX_TIME = 10
 
 # See: https://docs.google.com/a/google.com/spreadsheet/ccc?key=0AuX1PmdkirJmdGNWRlpOTDY3WjVNUkczR2pMVGtnS1E&hl=en_US#gid=0
 ZONES = {
-  1: {"nth": 0, "location": "Back Yard", "name": "Garden", "gallons_per_minute": 1.50},
-  2: {"nth": 1, "location": "Back Yard", "name": "Lawn 1", "gallons_per_minute": 2.99},
-  3: {"nth": 2, "location": "Back Yard", "name": "Lawn 2", "gallons_per_minute": 5.46},
-  4: {"nth": 3, "location": "Back Yard", "name": "Lawn 3", "gallons_per_minute": 5.46},
-  5: {"nth": 4, "location": "Back Yard", "name": "Patio Plants", "gallons_per_minute": 0.52},
-  7: {"nth": 5, "location": "Front Yard", "name": "Left Side Lawn", "gallons_per_minute": 6.13},
-  8: {"nth": 6, "location": "Front Yard", "name": "Right Side Lawn", "gallons_per_minute": 6.13}
+  1: {"nth": 0, "location": "Back Yard", "name": "Garden",
+      "gallons_per_minute": 1.50,
+      "coordinates": "602,198,782,267,781,282,735,301,556,226,557,212",
+      "center": (670, 244)},
+  2: {"nth": 1, "location": "Back Yard", "name": "Lawn 1",
+      "gallons_per_minute": 2.99,
+      "coordinates": "497,210,793,329,726,363,428,238,472,222",
+      "center": (615, 279)},
+  3: {"nth": 2, "location": "Back Yard", "name": "Lawn 2",
+      "gallons_per_minute": 5.46,
+      "coordinates": "421,242,721,364,647,398,349,271",
+      "center": (531, 311)},
+  4: {"nth": 3, "location": "Back Yard", "name": "Lawn 3",
+      "gallons_per_minute": 5.46, "coordinates":
+      "574,426,536,409,535,383,474,382,313,318,313,284,345,269,641,397",
+      "center": (463, 343)},
+  5: {"nth": 4, "location": "Back Yard", "name": "Patio Plants",
+      "gallons_per_minute": 0.52,
+      "coordinates": "535,429,528,447,487,468,449,446,447,394,477,390,528,411",
+      "center": (493, 418)},
+  7: {"nth": 5, "location": "Front Yard", "name": "Left Side Lawn",
+      "gallons_per_minute": 6.13,
+      "coordinates": "148,354,294,410,148,475,2,413",
+      "center": (151, 404)},
+  8: {"nth": 6, "location": "Front Yard", "name": "Right Side Lawn",
+      "gallons_per_minute": 6.13,
+      "coordinates": "300,413,446,471,292,532,150,476",
+      "center": (304, 465)}
 }
 COST_PER_CUBIC_FOOT = 0.0252
 CUBIC_FEET_PER_GALLON = 0.134
