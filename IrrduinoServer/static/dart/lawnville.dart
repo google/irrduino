b49ac0333e1e6b4dab1db35a2374d7f6b3efb8fa
Shannon -jj Behrens <jjinux@gmail.com>
2012-01-09 21:59:43 -0800

    Animated watering now works

--- IrrduinoServer/static/dart/lawnville.dart
+++ IrrduinoServer/static/dart/lawnville.dart
@@ -1,6 +1,6 @@
 #import('dart:html');
 
-typedef void Continuation();
+typedef void Callback();
 
 /**
  * This is like FarmVille, but it actually works.
@@ -13,8 +13,9 @@ typedef void Continuation();
  * to talk to IrrduinoController.
  */
 class Lawnville {
-  final int TIMER_INTERVAL = 10000;      // 10 seconds
-  final int REPOSITION_DURATION = 1000;  // 1 second
+  final int HALF_SECOND = 500;
+  final int TIMER_INTERVAL = 10000;
+  final int REPOSITION_DURATION = 1000;
   final int DROID_WIDTH = 100;
   final int DROID_HEIGHT = 100;
   final int DROID_IDLE_X = 381;
@@ -24,15 +25,20 @@ class Lawnville {
   Queue _actionQueue;
   bool _waitingForTimer = false;
   ImageElement _droid;
+  Map<String, Callback> _animationCallbacks;
+  int _animationStartTime;
+  int _animationProgress;
   
   Lawnville() {
     _actionQueue = new Queue();
+    _animationCallbacks = new HashMap<String, Callback>();
+    _droid = document.query("#droid");
   }
 
   void _run() {
     document.title = "LawnVille in Dart!";
-    _droid = document.query("#droid");
     _handleZoneClicks();
+    _startAnimationLoop();
   }
   
   void _handleZoneClicks() {
@@ -79,30 +85,40 @@ class Lawnville {
     _water(action["zone"]);
   }
  
-  void _water(zone) {
-    window.console.log("Watering zone: ${zone}");
-    AreaElement zone = document.query("#${zone}");
+  void _water(zoneId) {
+    window.console.log("Watering zone: ${zoneId}");
+    AreaElement zone = document.query("#${zoneId}");
     int x = Math.parseInt(zone.dataAttributes["center-x"]);
     int y = Math.parseInt(zone.dataAttributes["center-y"]);
     _repositionDroid(x, y, () {
       _droid.src = "/static/images/droid-watering-1.png";
+      _animationCallbacks["_wateringAnimation"] = _wateringAnimation;
     });
+  }
+  
+  void _wateringAnimation() {
+    if (!_droid.src.contains("/static/images/droid-watering-")) {
+      _animationCallbacks.remove("_wateringAnimation");
+    } else {
+      int oneOrTwo = (_animationProgress / HALF_SECOND).toInt() % 2 + 1;
+      _droid.src = "/static/images/droid-watering-${oneOrTwo}.png";
+    }
   }
   
   /**
    * Move the droid in an animated way.
    *
-   * If continuation is not null, then call it once the movement is done.
+   * If callback is not null, then call it once the movement is done.
    */
-  void _repositionDroid(int x, int y, [Continuation continuation = null]) {
+  void _repositionDroid(int x, int y, [Callback callback = null]) {
     x -= (HALF * DROID_WIDTH).toInt();
     y -= DROID_HEIGHT;
     _droid.src = "/static/images/droid-jetpack-on-front.png";
     _droid.style.left = "${x}px";
     _droid.style.top = "${y}px";
     window.setTimeout(() {
-      if (continuation != null) {
-        continuation();
+      if (callback != null) {
+        callback();
       } else {
         _droid.src = "/static/images/droid-waiting-front.png";
       }
@@ -112,6 +128,19 @@ class Lawnville {
   void _idle() {
     _repositionDroid(DROID_IDLE_X, DROID_IDLE_Y);
     window.console.log("I'm bored");
+  }
+  
+  void _startAnimationLoop() {
+    _animationStartTime = new Date.now().value;    
+    window.webkitRequestAnimationFrame(_animationLoop);
+  }
+  
+  void _animationLoop(int timestamp) {
+    _animationProgress = timestamp - _animationStartTime;
+    for (Callback callback in _animationCallbacks.getValues()) {
+      callback();
+    }
+    window.webkitRequestAnimationFrame(_animationLoop);
   }
 }
 
