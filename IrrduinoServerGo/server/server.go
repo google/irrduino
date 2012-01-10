6bc48c551b6ee5b6ffcee920ade322f1880ef28d
Shannon -jj Behrens <jjinux@gmail.com>
2012-01-10 15:20:28 -0800

    Cleaned up the copyrights

--- IrrduinoServerGo/server/server.go
+++ IrrduinoServerGo/server/server.go
@@ -1,3 +1,17 @@
+// Copyright 2007 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package server
 
 import (
@@ -13,22 +27,6 @@ func hello(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>Hello, world</h1>")
 }
 
-//#!/usr/bin/env python
-//#
-//# Copyright 2007 Google Inc.
-//#
-//# Licensed under the Apache License, Version 2.0 (the "License");
-//# you may not use this file except in compliance with the License.
-//# You may obtain a copy of the License at
-//#
-//#     http://www.apache.org/licenses/LICENSE-2.0
-//#
-//# Unless required by applicable law or agreed to in writing, software
-//# distributed under the License is distributed on an "AS IS" BASIS,
-//# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-//# See the License for the specific language governing permissions and
-//# limitations under the License.
-//#
 //from google.appengine.ext import webapp
 //from google.appengine.ext.webapp import util
 //
