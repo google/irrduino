9ec6b00cfd885a7ce7fbde8da2f6d56a6c874ee0
Shannon -jj Behrens <jjinux@gmail.com>
2012-01-10 20:01:13 -0800

    Misspelled

--- IrrduinoServerGo/server/handler/lawnville.go
+++ IrrduinoServerGo/server/handler/lawnville.go
@@ -21,7 +21,7 @@ import (
 
 // This is like FarmVille, but it actually works.
 func LawnVille(w http.ResponseWriter, r *http.Request) {
-	tmpl.TemplateSet.Execute(w, "Lawnville", nil)
+	tmpl.TemplateSet.Execute(w, "LawnVille", nil)
 }
 
 //from google.appengine.ext import webapp
